3c45c9d62451825b605b96636a5c0a94f4ca8727
Alex <ngang@bxscience.edu>
2017-08-12 07:46:21 -0400

    Revert "Removed node_modules"
    This reverts commit dc42dad5a8d883c741b67a3f1521061898668349.

---
+++ test.js
@@ -0,0 +1,24 @@
+/*
+  test.js
+  Author: Alex (aka Morty)
+  TEST SCRIPT.
+*/
+
+const Discord = require('discord.js');  //bot runs on the Discord js module
+const bot = new Discord.Client();
+const config = require("./config.json");  //Allows config details to be stored secretly
+const fs = require("fs");
+
+//To ensure that client  is ready. Small amount of time for discord.js to load its servers, users, etc.
+bot.on('ready', () => {
+  bot.user.setGame(`TEST SCRIPT with ${bot.users.size} friends!`);
+});
+
+bot.on('message', msg => {
+  // Exit and stop if the prefix is not there or if user is a bot.
+  if ((msg.content.startsWith(config.prefix)&&msg.content.equals("ping")) && !msg.author.bot) {
+    msg.reply('Pong!');
+  }else{return;}
+});
+
+bot.login(config.token);  //Logs in bot by fetching token from config file.
